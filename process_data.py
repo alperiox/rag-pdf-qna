af7c5d5c8124b74c8187cd5b1fcc931f470351d4
Alper Balbay <itsc0508@gmail.com>
2024-06-06 22:38:48 +0300

    ruff formatting

--- process_data.py
+++ process_data.py
@@ -8,6 +8,6 @@ def process_pdf(file) -> list[Document]:
     with tempfile.NamedTemporaryFile(delete=False) as f:
         f.write(file.getvalue())
         loader = PyMuPDFLoader(f.name, extract_images=True)
-    
+
     data = loader.load()
     return data
