832301dbe085b1870f669c67cfdfd02bdac2cbb3
Alper Balbay <itsc0508@gmail.com>
2024-06-06 22:33:55 +0300

    Fixed the streamlit UI
    - added the file uploader callback to set the selected document
    - added a logger for debugging the application
    - storing every required session-based variable in streamlit's session state
    - moved the sidebar to the last, so it can actually fetch the latest uploaded document too
    - vector databases are saved automatically to the disk and are loaded back when required

--- app.py
+++ app.py
@@ -1,57 +1,165 @@
+import logging
 import os
+import uuid
 
 import streamlit as st
 from dotenv import load_dotenv
 
-from chatbot import get_conversational_rag_chain, set_up_vectordb
+from chatbot import get_conversational_rag_chain, history, set_up_vectordb
 from process_data import process_pdf
 
+# set up the environment
 load_dotenv()
 
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+
+
+def set_up(dbname, file=None):
+    bar = st.progress(0, text="Processing PDF...")
+
+    logger.info(f"SET_UP: {dbname}, {file}")
+
+    if file:
+        logger.info("SET_UP: Processing PDF")
+        docs = process_pdf(file)
+    else:
+        docs = None
+
+    bar.progress(0.33, text="Setting up Chroma Vector DB...")
+
+    logger.info("SET_UP: Setting up Chroma Vector DB")
+    retriever = set_up_vectordb(dbname=dbname, docs=docs)
+
+    bar.progress(0.66, text="Setting up the conversational RAG chain...")
+
+    # set up the conversational RAG chain
+    logger.info("SET_UP: Setting up the conversational RAG chain")
+    rag_chain = get_conversational_rag_chain(retriever)
+
+    bar.progress(1.0, text="Done!")
+    return rag_chain
+
+
+def get_vector_store_name(filename):
+    for k in st.session_state.uploaded_docs:
+        if k[0] == filename:
+            return k[1]
+    return None
+
+
+def get_filename_from_vector_store(dbname):
+    for k in st.session_state.uploaded_docs:
+        if k[1] == dbname:
+            return k[0]
+    return None
+
+
+def callback():
+    """Callback function for the file uploader, sets the selected document."""
+    st.session_state.selected_doc = st.session_state["file_uploader"].name
+    return
+
+
+## RAG set up
+
+# set up the initial variables
 api_key = os.getenv("GOOGLE_API_KEY")
 
+if "uploaded_docs" not in st.session_state:
+    st.session_state["uploaded_docs"] = []
 
-with st.sidebar:
-    api_key = st.text_input("GCP API Key", key="chatbot_api_key", type="password", value=api_key)
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+if "loaded_vector_store" not in st.session_state:
+    st.session_state["loaded_vector_store"] = None
 
+if "selected_doc" not in st.session_state:
+    st.session_state["selected_doc"] = None
+
+if "rag_chain" not in st.session_state:
+    st.session_state["rag_chain"] = None
+
+logger.info(f"uploaded_docs: {st.session_state.uploaded_docs}")
+logger.info(f"loaded_vector_store: {st.session_state.loaded_vector_store}")
+logger.info(f"selected_doc: {st.session_state.selected_doc}")
+
+# right-side, main content
 st.title("💬 Talk with PDFs!")
 
-uploaded_file = st.file_uploader("Upload a PDF", type=("pdf"))
-# set up the vector database 
-if uploaded_file:
-    if not api_key:
-        st.info("Please add your GCP API key to continue.")
-        st.stop()
-
-    bar = st.progress(0, text="Processing PDF...")
-    docs = process_pdf(uploaded_file)
-    bar.progress(33.0, text="Setting up Chroma Vector DB...")
-    retriever = set_up_vectordb(docs)
-    bar.progress(66.0, text="Setting up the conversational RAG chain...")
-    # set up the conversational RAG chain
-    rag_chain = get_conversational_rag_chain(retriever)
-    bar.progress(100.0, text="Done!")
-
+uploaded_file = st.file_uploader(
+    "Upload a PDF", type=("pdf"), key="file_uploader", on_change=callback
+)
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hello! How can I help you?"}]
-
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "Hello! How can I help you?"}
+    ]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 
-if prompt := st.chat_input(placeholder="Can you give me a short summary?", disabled = not uploaded_file):
+logger.info(f"uploaded_file: {uploaded_file}")
+# set up the vector database
+if uploaded_file:
+    if not api_key:
+        st.info("Please add your GCP API key to continue.")
+        st.stop()
+
+    filename = uploaded_file.name
+
+    if filename not in [doc[0] for doc in st.session_state.uploaded_docs]:
+        logger.info(
+            "File is not in the uploaded docs list. Setting up the vector store."
+        )
+        dbname = uuid.uuid4().hex
+        st.session_state.uploaded_docs.append((filename, dbname))
+        rag_chain = set_up(dbname=dbname, file=uploaded_file)
+        st.session_state.loaded_vector_store = dbname
+        st.session_state.rag_chain = rag_chain
+
+        logger.info(f"new uploaded_docs: {st.session_state.uploaded_docs}")
+        logger.info(f"new loaded_vector_store: {st.session_state.loaded_vector_store}")
+        logger.info(f"new selected_doc: {st.session_state.selected_doc}")
+        logger.info(f"dbname: {dbname}")
+
+# if the currently selected document's vector store is not loaded, load it
+if st.session_state.selected_doc != get_filename_from_vector_store(
+    st.session_state.loaded_vector_store
+):
+    logger.info("Selected document's vector store is not loaded. Loading it.")
+    logger.info(f"selected_doc: {st.session_state.selected_doc}")
+    logger.info(f"loaded_vector_store: {st.session_state.loaded_vector_store}")
+    st.session_state.rag_chain = set_up(dbname=st.session_state.loaded_vector_store)
+
+## chat interface set up
+# print the LLM history object
+logging.info(f"\n history: \n {history} \n")
+
+if prompt := st.chat_input(
+    placeholder="Can you give me a short summary?", disabled=not uploaded_file
+):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    # get the response from the chain 
-    response = rag_chain.invoke(
-        {
-            "input": prompt
-        },
+    # get the response from the chain
+    response = st.session_state.rag_chain.invoke(
+        {"input": prompt},
         config={"configurable": {"session_id": "0"}},
-    )['answer']
+    )["answer"]
 
     st.session_state.messages.append({"role": "assistant", "content": response})
     st.chat_message("assistant").write(response)
+
+
+# sidebar
+
+with st.sidebar:
+    api_key = st.text_input(
+        "GCP API Key", key="chatbot_api_key", type="password", value=api_key
+    )
+
+    if st.session_state.uploaded_docs:
+        st.radio(
+            label="Recent documents",
+            options=[doc[0] for doc in st.session_state.uploaded_docs],
+            key="selected_doc",
+        )
--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,11 @@
+import os
+
 from dotenv import load_dotenv
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.documents import Document
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_google_vertexai import ChatVertexAI, VertexAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -12,16 +15,30 @@ from prompts import contextualize_q_prompt, qa_prompts
 load_dotenv()
 llm = ChatVertexAI(model="gemini-pro")
 history = ChatMessageHistory()
+embds = VertexAIEmbeddings(model_name="textembedding-gecko")
 
 
-def set_up_vectordb(docs):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(docs)
-    vectorstore = Chroma.from_documents(
-        documents=splits, embedding=VertexAIEmbeddings(model_name="textembedding-gecko")
-    )
-    retriever = vectorstore.as_retriever()
-    return retriever
+def set_up_vectordb(dbname: str, docs: list[Document] = None):
+    # check if the database exists
+    if os.path.exists(os.path.join("vectordbs", dbname)):
+        vectorstore = Chroma(
+            persist_directory=os.path.join("vectordbs", dbname),
+            embedding_function=embds,
+        )
+        retriever = vectorstore.as_retriever()
+        return retriever
+    else:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        splits = text_splitter.split_documents(docs)
+        vectorstore = Chroma.from_documents(
+            documents=splits,
+            embedding=embds,
+            persist_directory=os.path.join("vectordbs", dbname),
+        )
+        retriever = vectorstore.as_retriever()
+        return retriever
 
 
 def get_conversational_rag_chain(vdb_retriever):
